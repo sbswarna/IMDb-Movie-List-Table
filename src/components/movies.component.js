5885ebeab3585af4a864a543b0245d7bfaf5a145
Shatabdi Biswas Swarna <79458082+sbswarna@users.noreply.github.com>
2022-01-17 23:02:06 +0600

    Update movies.component.js

--- src/components/movies.component.js
+++ src/components/movies.component.js
@@ -2,58 +2,87 @@ import React, { Component } from "react";
 import Table from "./common/table.component";
 import Rating from "./rating.component";
 import getMovies from "../service/get-movies.service";
+import _ from "lodash";
 
 class Movies extends React.Component {
-    state = {
-        movies: []        
-    }
+  state = {
+    movies: [],
+    sortColumn: { path: "id", order: "asc" },
+  };
 
-    componentDidMount() {
-      const movies = getMovies();
-      this.setState({ movies });
-    }
+  componentDidMount() {
+    const movies = getMovies();
+    this.setState({ movies });
+  }
 
-    handleToggleRating = movieRank => {
-      const movies = [...this.state.movies];
-      const movie = movies.find(movie => movie.id === movieRank)
-      movie.your_rating = !(movie.your_rating);
-      this.setState({ movies });
-    }
+  handleToggleRating = (movieRank) => {
+    const movies = [...this.state.movies];
+    const movie = movies.find((movie) => movie.id === movieRank);
+    movie.your_rating = !movie.your_rating;
+    this.setState({ movies });
+  };
+
+  handleSort = (sortColumn) => {
+    this.setState({ ...this.state, sortColumn });
+  };
+
+  sortMovies = (movies) => {
+    const {sortColumn} = this.state;
+    const sortedMovies = _.orderBy(movies, [sortColumn.path], [sortColumn.order]);
+    return sortedMovies;
+  } 
 
   render() {
+    const movies = this.sortMovies(this.state.movies);
     const columns = [
-        {
-          label: 'Rank', 
-          path: 'id', 
-          content: (movie,key) => <td>{movie[key]}</td> 
-        },
-        {
-          label: 'Title', 
-          path: 'title', 
-          content:(movie,key) => <td>{movie[key]}</td> 
-        },
-        {
-          label: 'Poster', 
-          path: 'posterUrl', 
-          content: (movie,key) => <td><img src={movie[key]} style={{ height: "100px", width: "auto" }}/></td> 
-        },
-        {
-          label: 'Your Rating', 
-          path: 'your_rating', 
-          content: (movie,key) => <td><Rating 
-                                        isRated={movie[key]} 
-                                        rank={movie.id} 
-                                        handleToggleRating={this.handleToggleRating}/>
-                                  </td> },
-        {
-          label: 'Action', 
-          path: 'action', 
-          content: (movie,key) => <td>{movie[key]}</td> 
-        }
-    ]
+      {
+        label: "Rank",
+        path: "id",
+        sorting: true,
+        content: (movie, key) => <td>{movie[key]}</td>,
+      },
+      {
+        label: "Title",
+        path: "title",
+        sorting: true,
+        content: (movie, key) => <td>{movie[key]}</td>,
+      },
+      {
+        label: "Poster",
+        path: "posterUrl",
+        content: (movie, key) => (
+          <td>
+            <img src={movie[key]} style={{ height: "100px", width: "auto" }} />
+          </td>
+        ),
+      },
+      {
+        label: "Your Rating",
+        path: "your_rating",
+        content: (movie, key) => (
+          <td>
+            <Rating
+              isRated={movie[key]}
+              rank={movie.id}
+              handleToggleRating={this.handleToggleRating}
+            />
+          </td>
+        ),
+      },
+      {
+        label: "Action",
+        path: "action",
+        content: (movie, key) => <td>{movie[key]}</td>,
+      },
+    ];
     return (
       <>
-        <Table data={this.state.movies} columns={columns}/>
+        <Table
+          items={movies}
+          columns={columns}
+          onSort={this.handleSort}
+          sortColumn={this.state.sortColumn}
+        />
       </>
     );
   }
