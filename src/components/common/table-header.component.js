b3a1a5f0b5f36333ff8a16a57b1ed95aab7bfbd0
Shatabdi Biswas Swarna <79458082+sbswarna@users.noreply.github.com>
2022-01-17 23:02:34 +0600

    Update table-header.component.js

--- src/components/common/table-header.component.js
+++ src/components/common/table-header.component.js
@@ -1,12 +1,35 @@
-const TableHeader = props => {
+const TableHeader = ({ columns, onSort, sortColumn }) => {
+  //sortColumn = { path: id, order: desc/asc }
+  const handleSort = ({path, sorting}) => {
+    if(!sorting) return;
+
+    if (sortColumn.path === path) {
+      if (sortColumn.order === "asc") {
+        onSort({ path, order: "desc" });
+      } else {
+        onSort({ path, order: "asc" });
+      }
+    } else {
+      onSort({ path, order: "asc" });
+    }
+  };
+
+  const getIcon = (path) => {
+    if (sortColumn.path === path) {
+      if (sortColumn.order === "asc") {
+        return <i className="bi bi-sort-down"></i>;
+      } else return <i className="bi bi-sort-down-alt"></i>;
+    } else return null;
+  };
+
   return (
     <>
       <thead>
         <tr>
-          {props.columns.map((col) => {
+          {columns.map((col) => {
             return (
               <>
-                <th>{col.label}</th>
+                <th onClick={() => handleSort(col)}> {col.label} {getIcon(col.path)}</th>
               </>
             );
           })}
